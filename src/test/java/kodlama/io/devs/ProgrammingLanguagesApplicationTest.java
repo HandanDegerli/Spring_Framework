9eabbbc84c7ef48cd52303a9a772d578caf7331b
handan degerli <dgrl.handan@gmail.com>
2022-11-06 21:48:26 +0100

    changes

--- src/test/java/kodlama/io/devs/ProgrammingLanguagesApplicationTest.java
+++ src/test/java/kodlama/io/devs/ProgrammingLanguagesApplicationTest.java
@@ -1,6 +1,5 @@
 package kodlama.io.devs;
 
-import kodlama.io.devs.entity.concretes.ProgrammingLanguages;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,7 +8,5 @@ public class ProgrammingLanguagesApplicationTest {
 
     @Test
     void contextLoads(){
-
-
     }
 }
